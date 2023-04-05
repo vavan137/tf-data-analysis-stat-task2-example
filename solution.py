532704f10a7ebbd6be2b3a51d43b7d7e0e32bbb5
vavan137 <113305894+vavan137@users.noreply.github.com>
2023-04-05 03:29:52 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 from scipy.stats import expon
-
+from scipy.stats import norm
 
 chat_id = 5463739632 # Ваш chat ID, не меняйте название переменной
 
@@ -11,11 +11,14 @@ def solution(p: float, x: np.array) -> tuple:
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
     alpha = 1 - p
-    z1=expon.ppf(1-alpha/2)
-    z2=expon.ppf(alpha/2)
+    #z1=expon.ppf(1-alpha/2)
+    #z2=expon.ppf(alpha/2)
     #loc = x.mean()
     #scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
     #return loc - scale * norm.ppf(1 - alpha / 2), \
     #       loc - scale * norm.ppf(alpha / 2)
-    return x.mean()/np.sqrt(41*z1), \
-           x.mean()/np.sqrt(41*z2)
+    #return x.mean()/np.sqrt(41*z1), \
+    #       x.mean()/np.sqrt(41*z2)
+    left_b = x.mean() - np.sqrt(np.var(x)) * norm.ppf(1 - alpha / 2) / np.sqrt(len(x))
+    right_b = x.mean() - np.sqrt(np.var(x)) * norm.ppf(alpha / 2) / np.sqrt(len(x))
+    return (31*left_b)/np.sqrt(41)/39, (31*right_b)/np.sqrt(41)/39
