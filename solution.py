7bbbaf99b16625c4025673241c80873c8272331c
vavan137 <113305894+vavan137@users.noreply.github.com>
2023-04-06 17:45:09 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 
 from scipy.stats import expon
+from scipy.stats import chi2
 from scipy.stats import norm
 
 chat_id = 5463739632 # Ваш chat ID, не меняйте название переменной
@@ -22,6 +23,8 @@ def solution(p: float, x: np.array) -> tuple:
     #left_b = x.mean() - np.sqrt(np.var(x)) * norm.ppf(1 - alpha / 2) / np.sqrt(len(x))
     #right_b = x.mean() - np.sqrt(np.var(x)) * norm.ppf(alpha / 2) / np.sqrt(len(x))
     #return (31*left_b)/np.sqrt(41)/39, (31*right_b)/np.sqrt(41)/39
-    z1 = expon.ppf(1-alpha/2,scale=2)
-    z2 = expon.ppf(alpha/2,scale=2)
-    return np.sqrt(np.mean(x*x))/np.sqrt(82) - z2*4/np.sqrt(len(x)),  np.sqrt(np.mean(x*x))/np.sqrt(82) + z2*4/np.sqrt(len(x)) 
+    size = len(x)	
+	chi2_rv = chi2(df = 2 * size)
+	left_b = chi2_rv.ppf(1 - alpha / 2)
+	right_b = chi2_rv.ppf(alpha / 2)	
+	return np.sqrt(size * np.mean(x*x) / (left_b * 41)), np.sqrt(size * np.mean(x*x) / (right_b * 41))
