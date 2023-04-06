586d515c3e6a7e1a4bbcc897edeae1c6e91824c2
vavan137 <113305894+vavan137@users.noreply.github.com>
2023-04-06 17:46:14 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -24,7 +24,7 @@ def solution(p: float, x: np.array) -> tuple:
     #right_b = x.mean() - np.sqrt(np.var(x)) * norm.ppf(alpha / 2) / np.sqrt(len(x))
     #return (31*left_b)/np.sqrt(41)/39, (31*right_b)/np.sqrt(41)/39
     size = len(x)	
-	chi2_rv = chi2(df = 2 * size)
-	left_b = chi2_rv.ppf(1 - alpha / 2)
-	right_b = chi2_rv.ppf(alpha / 2)	
-	return np.sqrt(size * np.mean(x*x) / (left_b * 41)), np.sqrt(size * np.mean(x*x) / (right_b * 41))
+    chi2_rv = chi2(df = 2 * size)
+    left_b = chi2_rv.ppf(1 - alpha / 2)
+    right_b = chi2_rv.ppf(alpha / 2)	
+    return np.sqrt(size * np.mean(x*x) / (left_b * 41)), np.sqrt(size * np.mean(x*x) / (right_b * 41))
